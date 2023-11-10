50b9cbc9afdda3b1f08e7a1af01bbc55fda4fe33
aalexebr <alex.ebr@icloud.com>
2023-11-10 11:15:56 +0100

    complete

--- src/org/lessons/java/snack1.java
+++ src/org/lessons/java/snack1.java
@@ -1,7 +1,23 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class snack1 {
 	public static void main(String[] args) {
-		System.out.println("test");
+//		System.out.println("test");
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("give me a number: ");
+		String userInput = in.nextLine();
+		in.close();
+		int userNumber = Integer.valueOf(userInput+"");
+		
+		if(userNumber % 2 == 0) {
+			System.out.print("print number is even so next numb is: "+(userNumber+1));
+		}
+		else {
+			System.out.print("print number: "+userNumber);
+		}
+		
 	}
 }
